478ebab94aa69cc93870861cd62a33eb5f0e4ae9
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-09-10 14:36:38 +0800

    fix template

---
+++ test/resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layout')
+
+@section('title','Dashboard')
+@section('page', 'Dashboard')
+
+@section('breadcrumps')
+<div class="col-md-7 align-self-center">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Menus</li>
+        <li class="breadcrumb-item">Dashboard</li>
+        <li class="breadcrumb-item active">View</li>
+    </ol>
+</div>
+@endsection
+
+@section('content')
+    <h1>dashboard page</h1>
+@endsection
---
+++ test/resources/views/layouts/nav.blade.php
@@ -0,0 +1,22 @@
+<nav class="sidebar-nav">
+    <ul id="sidebarnav">
+        
+        <li class="nav-devider"></li>
+        <li class="nav-small-cap">Menus</li>
+        <li> 
+            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard</span></a>
+            <ul aria-expanded="false" class="collapse">
+                <li><a href="/">View </a></li>
+            </ul>
+        </li>
+
+
+        <li> 
+            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">Users</span></a>
+            <ul aria-expanded="false" class="collapse">
+                <li><a href="/users">Manage </a></li>
+            </ul>
+        </li>
+        
+    </ul>
+</nav>
